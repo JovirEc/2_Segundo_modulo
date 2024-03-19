3548a91163b022f36aba70888e01b18d2a9fe259
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-19 12:25:25 -0500

    Paso 5 completado

---
+++ Workspace_tareas/EvaluacionJVR/src/com/clearminds/componentes/Celda.java
@@ -0,0 +1,41 @@
+package com.clearminds.componentes;
+
+public class Celda {
+//ATRIBUTOS
+	private Producto producto;
+	private int stock;
+	private String codigo;
+	
+//CONSTRUCTOR
+	public Celda(String codigo) {
+		this.codigo = codigo;
+	}
+	
+//GETTERS SETTERS
+	public Producto getProducto() {
+		return producto;
+	}
+	
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+//METODOS
+	public void ingresarProducto (Producto prod1, int stck1) {
+		producto = prod1;
+		stock = stck1;
+	}
+	
+	
+}
