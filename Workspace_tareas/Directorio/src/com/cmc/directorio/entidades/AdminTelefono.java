53d418bd05991d67c23f6752415a5265ea4914ba
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-18 21:01:17 -0500

    Ejercicio 2 - Paso 1 - Agregar metodo CantarMovi

--- Workspace_tareas/Directorio/src/com/cmc/directorio/entidades/AdminTelefono.java
+++ Workspace_tareas/Directorio/src/com/cmc/directorio/entidades/AdminTelefono.java
@@ -7,5 +7,21 @@ public class AdminTelefono {
 			telf.setTieneWhatsapp(true);
 		}
 	}
+	
+	public int contarMovi(Telefono telf1, Telefono telf2, Telefono telf3) {
+		int contador = 0;
+		
+		if(telf1.getOperadora() == "Movi") {
+			contador = contador + 1;
+		}
+		if(telf2.getOperadora() == "Movi") {
+			contador = contador + 1;
+		}
+		if(telf3.getOperadora() == "Movi") {
+			contador = contador + 1;
+		}
+		
+		return contador;
+	}
 
 }
