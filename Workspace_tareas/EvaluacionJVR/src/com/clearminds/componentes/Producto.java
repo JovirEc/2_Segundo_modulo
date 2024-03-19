1ce98d1f118c16fb1929708c679962baed432307
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-19 12:18:26 -0500

    Paso 4 comprobado, todo ok.

--- Workspace_tareas/EvaluacionJVR/src/com/clearminds/componentes/Producto.java
+++ Workspace_tareas/EvaluacionJVR/src/com/clearminds/componentes/Producto.java
@@ -1,5 +1,44 @@
 package com.clearminds.componentes;
 
 public class Producto {
+//ATRIBUTOS
+	private String nombre;
+	private double precio;
+	private String codigo;
+//CONSTRUCTOR
+	public Producto(String codigo, String nombre, double precio) {
+	this.nombre = nombre;
+	this.precio = precio;
+	this.codigo = codigo;
+	}
+//GETTERS SETTERS
+	public String getNombre() {
+		return nombre;
+	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+//METODOS
+	public void incrementarPrecio(int porcentaje) {
+		double valorAgregado =(precio * porcentaje) / 100;
+		this.precio = precio + valorAgregado;
+	}
+	
+	public void disminuirPrecio(double descuento) {
+		this.precio = precio - descuento;
+	}
+	
 }
