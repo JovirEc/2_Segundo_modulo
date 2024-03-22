62f8b41dcd99b8edbca57dfc0e6ca3aa371fe813
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-22 18:12:43 -0500

    Avance 7

---
+++ Workspace_tareas/JavaJoelVillamar/src/com/krakedev/evaluacion/Directorio.java
@@ -0,0 +1,44 @@
+package com.krakedev.evaluacion;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Directorio {
+
+	private ArrayList<Contacto> contactos;
+	private Date fechaModificacion;
+	
+	//CONSTRUCTOR
+	public Directorio() {
+		contactos = new ArrayList<Contacto>();
+	}
+	
+	//GETTERS SETTERS
+	public ArrayList<Contacto> getContactos() {
+		return contactos;
+	}
+	public Date getFechaModificacion() {
+		return fechaModificacion;
+	}
+	public void setFechaModificacion(Date fechaModificacion) {
+		this.fechaModificacion = fechaModificacion;
+	}
+	
+	//METODOS
+	public boolean agregarContacto(Contacto contacto) {
+		contactos.add(contacto);
+		return true;
+	}
+	
+	public Contacto buscarPorCedula(String cedula) {
+		String cedulaX;
+		for(Contacto contacto : contactos) {
+			cedulaX = contacto.getCedula();
+			if(cedula.equals(cedulaX)) {
+				return contacto;
+			}
+		}
+		return null;
+	}
+	
+}
