64fa63848dd13fb626709a93ca3192b1e4984c5d
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-21 23:46:04 -0500

    Clase Curso - Metodo buscarEstudiantePorCedula OK - Metodo MatricularEstudiante OK - metodo Mostrar OK

--- Workspace_tareas/Notas/src/com/krakedev/test/TestPrueba.java
+++ Workspace_tareas/Notas/src/com/krakedev/test/TestPrueba.java
@@ -9,29 +9,52 @@ public class TestPrueba {
 
 	public static void main(String[] args) {
 		
-		//Curso curso1 = new Curso();
+		//ESTUDIANTE 1
 		Estudiante estudiante1 = new Estudiante("1234567890", "Juan2", "Perez");
 		Materia sociales= new Materia("COD1", "Sociales");
-		Materia ciencias= new Materia("FOD2", "Ciencias");
+		Materia ciencias= new Materia("FOD1", "Ciencias");
 		Nota nota1 = new Nota(sociales, 8.0);
 		Nota nota2 = new Nota(ciencias, 9.0);
 		
 		estudiante1.agregarNota(nota1);
 		estudiante1.agregarNota(nota2);
-		
 		estudiante1.mostrar();
-		estudiante1.modificarNota("FOD2", 4.0);
+		
+		estudiante1.modificarNota("FOD1", 4.0);
+		System.out.println("--Despues de Modificar--");
+		estudiante1.mostrar();
+		
+		double promedioEstudiante1 = estudiante1.calcularPromedioNotasEstudiante();
+		System.out.println("Promedio estudiante: "+promedioEstudiante1);
+		
+		//ESTUDIANTE 2
+		Estudiante estudiante2 = new Estudiante("1234567891", "Adrian", "Chalén");
+		Materia politica= new Materia("COD2", "Politica");
+		Materia economia= new Materia("FOD2", "Economia");
+		Nota nota3 = new Nota(politica, 10.0);
+		Nota nota4 = new Nota(economia, 9.0);
 		
 		System.out.println("");
+		estudiante2.agregarNota(nota3);
+		estudiante2.agregarNota(nota4);
+		estudiante2.mostrar();
+		
+		estudiante2.modificarNota("FOD2", 6.0);
 		System.out.println("--Despues de Modificar--");
+		estudiante2.mostrar();
 		
-		estudiante1.mostrar();
+		double promedioEstudiante2 = estudiante2.calcularPromedioNotasEstudiante();
+		System.out.println("Promedio estudiante: "+promedioEstudiante2);
 		
-		double promedioEstudiante = estudiante1.calcularPromedioNotasEstudiante();
-		System.out.println("Promedio estudiante: "+promedioEstudiante);
-		/*estudiante.agregarNota(nota1);
-		estudiante.modificarNota("3", 10.00);
-		estudiante.mostrar();*/
+		//CURSO PRUEBA METODOS
+		Curso paraleloA = new Curso();
+			//MATRICULAR
+		paraleloA.matricularEstudiante(estudiante1);
+		paraleloA.matricularEstudiante(estudiante2);
+		System.out.println("");
+		paraleloA.mostrar();
+		
+
 	}
 
 
