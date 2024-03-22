f3fa74868eb7fe754526784357e12ad759e7c39a
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-21 23:28:18 -0500

    Clase Estudiante - Metodo CalcularPromedioNotasEstudiantes Funcional

--- Workspace_tareas/Notas/src/com/krakedev/test/TestPrueba.java
+++ Workspace_tareas/Notas/src/com/krakedev/test/TestPrueba.java
@@ -13,19 +13,22 @@ public class TestPrueba {
 		Estudiante estudiante1 = new Estudiante("1234567890", "Juan2", "Perez");
 		Materia sociales= new Materia("COD1", "Sociales");
 		Materia ciencias= new Materia("FOD2", "Ciencias");
-		Nota nota1 = new Nota(sociales, 10.0);
+		Nota nota1 = new Nota(sociales, 8.0);
 		Nota nota2 = new Nota(ciencias, 9.0);
 		
 		estudiante1.agregarNota(nota1);
 		estudiante1.agregarNota(nota2);
 		
 		estudiante1.mostrar();
-		estudiante1.modificarNota("FOD2", 7.00);
+		estudiante1.modificarNota("FOD2", 4.0);
 		
 		System.out.println("");
 		System.out.println("--Despues de Modificar--");
 		
 		estudiante1.mostrar();
+		
+		double promedioEstudiante = estudiante1.calcularPromedioNotasEstudiante();
+		System.out.println("Promedio estudiante: "+promedioEstudiante);
 		/*estudiante.agregarNota(nota1);
 		estudiante.modificarNota("3", 10.00);
 		estudiante.mostrar();*/
