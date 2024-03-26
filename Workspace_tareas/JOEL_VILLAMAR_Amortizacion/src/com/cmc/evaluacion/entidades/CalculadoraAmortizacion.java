10a72a5d81ad4698e62d730cb885de1d67dc006f
Jovir69 <jovirodriguezgye@gmail.com>
2024-03-26 00:25:21 -0500

    Se agregó el metodo MostrarPrestamo

---
+++ Workspace_tareas/JOEL_VILLAMAR_Amortizacion/src/com/cmc/evaluacion/entidades/CalculadoraAmortizacion.java
@@ -0,0 +1,19 @@
+package com.cmc.evaluacion.entidades;
+
+public class CalculadoraAmortizacion {
+	
+	//METODOS
+	public static double calcularCuota(Prestamo prestamo) {
+
+		double monto = prestamo.getMonto();
+		double i = prestamo.getInteres();
+			double interes = (i/12)/100;
+		int plazo = prestamo.getPlazo();
+		double cuota;
+		
+		cuota = (monto * interes)/(1- Math.pow((1+interes),(-plazo)));
+		
+		return cuota;
+	}
+	
+}
