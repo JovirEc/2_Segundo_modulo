365577efd2c1dbc8cd310a4e3873af723d4a3add
Jovir69 <jovirodriguezgye@gmail.com>
2024-04-09 12:02:11 -0500

    Ejercicios con Maven

---
+++ Workspace_ejercicios/ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,14 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	
+	public int sumar(int num1,int num2) {
+		int suma = num1 + num2;
+		return suma;
+	}
+	
+	public int restar(int num1,int num2) {
+		int resta = num1 - num2;
+		return resta;
+	}
+}
